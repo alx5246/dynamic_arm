ca1c52e7d0224f75a4dff7e05a658cf7feb12433
alx5246 <ajl17@case.edu>
2017-03-18 12:13:03 -0400

    Finished version of src/arm_model.m, but it is not yet tested and we still need some-way to generate intial muscle lengths.

--- src/arm_model.m
+++ src/arm_model.m
@@ -1,6 +1,26 @@
-function [theta_1, theta_dot_1, theta_2, theta_dot_2, theta_3, theta_dot_3] = arm_model(theta_1, theta_2, theta_3)
+function [theta_1_new, theta_1_dot_new, theta_2_new, theta_2_dot_new] = arm_model(theta_1, theta_1_dot, theta_2, theta_2_dot, alpha, dt)
 
-% Setting up arm parameters (excluding muscles)
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% INPUTS
+% alpha: 9x1 vector of muscle activations over domain 0 <= aplha_i <= 1
+%     where alpha actually equals .... 
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% STATE BOUNDS
+% I think we need bounds on the arm (as would be in physical reality) or 
+% the arm dynamics at some point will break down. We may also have to
+% sufficienlty alter the dynamics so we do not violate these bounds.
+theta_1_min = 30.0*(pi/180);
+theta_1_max = 150.0*(pi/180);
+theta_2_min = 30.0*(pi/180);
+theta_2_max = 150.0*(pi/180);
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Arm parameters (excluding muscles)
 
 % Upper arm (length, mass, Intertia, and mass-center)
 L_1 = 0.310; % (m)
@@ -14,13 +34,11 @@ m_2 = 1.3200; % (kg)
 I_2 = 0.0120; % (kg * m^2)
 mc_2 = 0.1350; % (m) 
 
-% Wrist arm (length, mass, Intertia, and mass-center)
-L_3 = 0.1500; % (m)
-m_3 = 0.3500; % (kg)
-I_3 = 0.0010; % (kg * m^2)
-mc_3 = 0.057; % (m) 
 
-% Setting up muscle parameters as used in original paper!
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Muscle parameters 
+
+% Muscle attachment points (as given in the original paper.
 
 % Muscle l-1
 r_1 = 0.055; % (m)
@@ -46,17 +64,19 @@ w_52 = 0.045; % (m)
 w_61 = 0.040; % (m)
 w_62 = 0.045; % (m)
 
-% Muscle l-7
-r_7 = 0.035; % (m)
-s_7 = 0.220; % (m)
+% Muscle rest lengths, these values NEED to be set by the user and is the 
+% rest length of the muscles in the system
+l_0 = .1 * ones(6,1);
 
-% Muscle l-8
-r_8 = 0.050; % (m)
-s_8 = 0.225; % (m)
+% Alpha parameters, these are the f_0 values that scale up our muslce
+% forces
+f_0 = 20 * ones(6,1); % (newtons)
 
-% Muscle l-9
-w_91 = 0.040; % (m)
-w_92 = 0.030; % (m)
+% Muscle intrinsic viscosity c_0 as set by the paper is constant and
+c_0 = .2 * ones(6,1);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Muscle length changes 
 
 % Finding lengths of the muscles l-1 through l-9
 l = [ (r_1^2 + s_1^2 + 2*r_1*s_1*cos(theta_1))^.5; ...
@@ -64,42 +84,94 @@ l = [ (r_1^2 + s_1^2 + 2*r_1*s_1*cos(theta_1))^.5; ...
       (r_3^2 + s_3^2 + 2*r_3*s_3*cos(theta_2))^.5; ...
       (r_4^2 + s_4^2 - 2*r_4*s_4*cos(theta_2))^.5; ...
       (w_51^2 + w_52^2 + L_1^2 + 2*w_51*L_1*cos(theta_1) + 2*w_52*L_1*cos(theta_2) + 2*w_51*w_52*cos(theta_1 + theta_2))^.5; ...
-      (w_61^2 + w_62^2 + L_1^2 - 2*w_61*L_1*cos(theta_1) - 2*w_62*L_1*cos(theta_2) + 2*w_61*w_52*cos(theta_1 + theta_2))^.5; ...
-      (r_7^2 + s_7^2 + 2*r_7*s_7*cos(theta_3))^.5; ...
-      (r_8^2 + s_8^2 - 2*r_8*s_8*cos(theta_3))^.5; ...
-      (w_91^2 + w_92^2 + L_2^2 + 2*w_91*L_2*cos(theta_2) + 2*w_92*L_2*cos(theta_3) + 2*w_91*w_92*cos(theta_2 + theta_3))^.5];
+      (w_61^2 + w_62^2 + L_1^2 - 2*w_61*L_1*cos(theta_1) - 2*w_62*L_1*cos(theta_2) + 2*w_61*w_52*cos(theta_1 + theta_2))^.5];
   
 % Later we will need to understand how the lengths are chaning, in order to
 % do this we need the Jacobin of "l" with respect to the the values of 
 % the angles, this Jacobian will be called "W" and will be a 9x3 matrix
 W = [ .5*(r_1^2 + s_1^2 + 2*r_1*s_1*cos(theta_1))^(-.5) * (-2*r_1*s_1*sin(theta_1)), ...
-      0.0, ...
-      0.0; ...
+      0.0;
       .5*(r_2^2 + s_2^2 - 2*r_2*s_2*cos(theta_1))^(-.5) * (2*r_2*s_2*sin(theta_1)), ...
+      0.0;
       0.0, ...
-      0.0; ...
+      .5*(r_3^2 + s_3^2 + 2*r_3*s_3*cos(theta_2))^(-.5) * (-2*r_3*s_3*sin(theta_2));
       0.0, ...
-      .5*(r_3^2 + s_3^2 + 2*r_3*s_3*cos(theta_2))^(-.5) * (-2*r_3*s_3*sin(theta_2)), ...
-      0.0; ...
-      0.0, ...
-      .5*(r_4^2 + s_4^2 - 2*r_4*s_4*cos(theta_2))^(-.5) * (2*r_4*s_4*sin(theta_2)), ...
-      0.0; ...
+      .5*(r_4^2 + s_4^2 - 2*r_4*s_4*cos(theta_2))^(-.5) * (2*r_4*s_4*sin(theta_2));
       .5*(w_51^2 + w_52^2 + L_1^2 + 2*w_51*L_1*cos(theta_1) + 2*w_52*L_1*cos(theta_2) + 2*w_51*w_52*cos(theta_1 + theta_2))^(-.5) * (-2*w_51*L_1*sin(theta_1) - 2*w_51*w_52*sin(theta_1 + theta_2)), ...
-      .5*(w_51^2 + w_52^2 + L_1^2 + 2*w_51*L_1*cos(theta_1) + 2*w_52*L_1*cos(theta_2) + 2*w_51*w_52*cos(theta_1 + theta_2))^(-.5) * (-2*w_52*L_1*sin(theta_2) - 2*w_51*w_52*sin(theta_1 + theta_2)), ...
-      0.0; ...
+      .5*(w_51^2 + w_52^2 + L_1^2 + 2*w_51*L_1*cos(theta_1) + 2*w_52*L_1*cos(theta_2) + 2*w_51*w_52*cos(theta_1 + theta_2))^(-.5) * (-2*w_52*L_1*sin(theta_2) - 2*w_51*w_52*sin(theta_1 + theta_2));
       .5*(w_61^2 + w_62^2 + L_1^2 - 2*w_61*L_1*cos(theta_1) - 2*w_62*L_1*cos(theta_2) + 2*w_61*w_62*cos(theta_1 + theta_2))^(-.5) * (2*w_61*L_1*sin(theta_1) - 2*w_61*w_62*sin(theta_1 + theta_2)), ...
-      .5*(w_61^2 + w_62^2 + L_1^2 - 2*w_61*L_1*cos(theta_1) - 2*w_62*L_1*cos(theta_2) + 2*w_61*w_62*cos(theta_1 + theta_2))^(-.5) * (2*w_62*L_1*sin(theta_2) - 2*w_61*w_62*sin(theta_1 + theta_2)), ...
-      0.0; ...
-      0.0, ...
-      0.0, ...
-      .5*(r_7^2 + s_7^2 + 2*r_7*s_7*cos(theta_3))^(-.5) * (-2*r_7*s_7*sin(theta_3)); ...
-      0.0, ...
-      0.0, ...
-      .5*(r_8^2 + s_8^2 - 2*r_8*s_8*cos(theta_3))^(-.5) * (2*r_8*s_8*sin(theta_3)); ...
-      0.0, ...
-      .5*(w_91^2 + w_92^2 + L_2^2 + 2*w_91*L_2*cos(theta_2) + 2*w_92*L_2*cos(theta_3) + 2*w_91*w_92*cos(theta_2 + theta_3))^(-.5) * (-2*w_91*L_2*sin(theta_2) - 2*w_91*w_92*sin(theta_3 + theta_2)), ...
-      .5*(w_91^2 + w_92^2 + L_2^2 + 2*w_91*L_2*cos(theta_2) + 2*w_92*L_2*cos(theta_3) + 2*w_91*w_92*cos(theta_2 + theta_3))^(-.5) * (-2*w_92*L_2*sin(theta_3) - 2*w_91*w_92*sin(theta_3 + theta_2))];
-      
+      .5*(w_61^2 + w_62^2 + L_1^2 - 2*w_61*L_1*cos(theta_1) - 2*w_62*L_1*cos(theta_2) + 2*w_61*w_62*cos(theta_1 + theta_2))^(-.5) * (2*w_62*L_1*sin(theta_2) - 2*w_61*w_62*sin(theta_1 + theta_2))];
+
+% Now calculate rate of muscle lenth change
+l_dot = W*[theta_1_dot, theta_2_dot]; % should be a 6X1 vector
+
+ 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% MUSLCE FORCE CALCULATIONS
+
+% "alpha_hat" is the scaled up values of alphs
+alpha_hat = f_0 .* alpha; % shold be a 6x1 vector
+
+% "A" is the matrix version of "alpha_hat"
+A = diag(alpha_hat);
+
+% Calculate the "p" values
+p = (.9.*l_0)./(.9.*l_0 + abs(l_dot)); % should be a 6x1 vector
+
+% "P" is the matrix version of "p"
+P = diag(p);
+
+% Calculate the "c" values depending on sign of direction change
+c = zeros(6,1);
+for i=1:6
+    if l_dot(i) >= 0
+        c(i) = max( 0.25/(.9*l_0(i)), 0);
+    else
+        c(i) = max( 2.25/(.9*l_0(i)), 0);
+    end
+end
+  
+% "C" is the matrix version of "c"
+C = diag(c);
+
+% "C_0" is the matrix version of "c_0"
+C_0 = diag(c_0);
+
+% Fm are the forces applied by the muscles!
+Fm = P*alpha_hat - P*(A*C+C_0)*l_dot; % should be a 6X1 matrix 
+
+% Tm is the torque applied by the muscles
+Tm = W'*Fm; % should be a 2X1 matrix;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
+% Arm Dynamic Models
+
+% H*Theta_dot_dot + S*Theta_dot = Torque
+
+H_11 = m_1*(mc_1^2) + I_1 + m_2*(L_1^2 + mc_2^2 + 2*L_1*mc_2*cos(theta_2)) + I_2;
+H_12 = m_2*L_1*mc_2*cos(theta_2) + m_2*(mc_2^2) + I_2;
+H_21 = H12;
+H_22 = m_2*mc_2^2 + I_2;
+
+H = [H_11, H12; H21, H22];
+
+h = m_2*L_1*(mc_2^2)*sin(theta_2);
+
+S = [-h*theta_2_dot, -h*(theta_1_dot+theta_2_dot);...
+     h*theta_1_dot, 0.0];
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% ARM SIMULATION
+% Here we are going to apply Euler-One Step to find Theta_dot_dot 
+
+% Find Theta_dot approximation
+The_dots = [theta_1_dot; theta_2_dot] + inv(H)*(Tm - S*[theta_1_dot; theta_2_dot]) .* dt;
+theta_1_dot_new = The_dots(1);
+theta_2_dot_new = The_dots(2);
+
+The = [theta_1;theta_2] + The_dots.*dt;
+theta_1_new = The(1);
+theta_2_new = The(2);
       
       
 
